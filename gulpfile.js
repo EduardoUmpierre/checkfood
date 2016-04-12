64542ede11d973f165cc2615f068f906e9b1800d
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2016-04-12 01:18:35 -0300

    Aprimoramento no gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
- 
+
 gulp.task('sass', function () {
   return gulp.src('./src/sass/styles.scss')
     .pipe(sass.sync().on('error', sass.logError))
@@ -24,12 +24,7 @@ gulp.task('js', function() {
 });
 
 gulp.task('js:watch', function() {
-    gulp.watch('./src/js/**/*.js', function(event) {
-        gutil.log('File '+event.path+' was '+event.type+', running tasks...');
-        gulp.run('js');
-    });
+    gulp.watch('./src/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['sass:watch', 'sass', 'js', 'js:watch'], function() {
-  // place code for your default task here
-});
+gulp.task('default', ['sass:watch', 'sass', 'js', 'js:watch'], function() {});
